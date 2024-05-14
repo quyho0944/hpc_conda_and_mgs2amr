c0b985b47e28801078ed2f70920c5c4c847e0538
quyho0944 <163754479+quyho0944@users.noreply.github.com>
2024-05-14 18:41:50 +0700

    Update using_ascp.sh

--- using_ascp.sh
+++ using_ascp.sh
@@ -5,15 +5,19 @@ wget -qO- https://download.asperasoft.com/download/sw/connect/3.9.8/ibm-aspera-c
 # Then you run this code to install 
 ibm-aspera-connect-3.9.8.176272-linux-g2.12-64.sh
 
-# after installed, you can set path of this ascp tool in ~/bashrc and type ascp to run tool
+# After installed, you can set path of this ascp tool in ~/bashrc and type ascp to run tool
 nano ~/.bashrc
 export PATH=/path/to/your/ascp/:$PATH
+
+# Then you save ~/.bashrc and rerun it 
 source ~/.bashrc 
+
+# Check tools
 ascp -h 
 
-# like ssh we need keypair to download data from remote machine 
+# Like ssh we need keypair to download data from remote machine 
+# This is public key look like and you need to asign admind to get it 
 /home/hp/local/aspera/etc/asperaweb_id_dsa.openssh
-# this is public key look like and you need to asign admind to get it 
 
-# download using ascp
+# Download using ascp
 ascp -T -i /home/hp/local/aspera/etc/asperaweb_id_dsa.openssh anonftp@ftp.ncbi.nlm.nih.gov:/blast/db/nt.{000..001}.tar.gz/ ./
